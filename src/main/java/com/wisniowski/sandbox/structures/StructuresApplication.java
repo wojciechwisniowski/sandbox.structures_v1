718bed0a3fab07ea6d5d147394c9756d33cb4d9e
wojciech.wisniowski <wojciech.wisniowski@gmail.com>
2020-11-03 10:09:35 +0100

    init

---
+++ src/main/java/com/wisniowski/sandbox/structures/StructuresApplication.java
@@ -0,0 +1,13 @@
+package com.wisniowski.sandbox.structures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisniowski/sandbox/structures/StructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisniowski.sandbox.structures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
